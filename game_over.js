d9124b04dfb9c78b3dabf3444d40bf7abb0bf271
fflachat <florent.flachat@gmail.com>
2022-03-19 18:11:45 +0100

    work on JS forto setup the game, add gamepad control for mobile, display score in real time on the game page, and on the game over page

--- game_over.js
+++ game_over.js
@@ -1,6 +1,8 @@
 // Trick to go to the score page for the demo
-
 const btnScore = document.querySelector('#nameInputBtn');
+
+document.getElementById('score').innerHTML =
+  window.localStorage.getItem('Score');
 
 btnScore.addEventListener('click', (event) => {
   event.preventDefault();
