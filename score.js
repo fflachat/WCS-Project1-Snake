84c5ac5107778fd345ea7cf72d5e1f640a33ba7b
JVRasa <j.rasamoelina@gmail.com>
2022-03-23 15:58:21 +0100

    test du classement

--- score.js
+++ score.js
@@ -1,6 +1,6 @@
 const clickTest = document.querySelector('h1');
 const score = [1, 90, 4, 87, 43, 99, 77, 21];
-const firstPlayer = document.getElementById('firstPlayer');
+const firstPlayerName = document.getElementById('firstPlayerName');
 
 const player1 = {
   name: 'julie',
@@ -48,7 +48,7 @@ clickTest.addEventListener('click', () => {
   triClassemnt();
   for (let i = 0; i < score.length; i++) {
     if (score[i] === player1.score) {
-      firstPlayer.innerText = player1.name;
+      firstPlayerName.innerText = player1.name;
     }
   }
 });
