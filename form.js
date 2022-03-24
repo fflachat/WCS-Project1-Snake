fbb360121cda4948ac6802dd2c2a7f708fb72260
dialloth <althierno@yahoo.fr>
2022-03-24 11:23:50 +0100

    form files modified

--- form.js
+++ form.js
@@ -1,5 +1,25 @@
-/* eslint-disable no-alert */
+// const input = document.getElementById('input');
+// const form = document.getElementById('form');
+// const message = document.getElementById('message');
 
+
+// form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//     if (input.value === '' || input.value === null) {
+//             alert("Veuillez entrez un nom d'abord !");
+//         }
+//     else if
+//         (message.value === '' || message.value === null) {
+//             alert("veuillez aussi saisir un message, s'il vous plait");
+    
+//         }
+    
+//     else if(input.value !== '' && message.value !== null ){
+//             alert("Nous avons bien reçu votre message. Merci de nous avoir contacté !");
+//         }
+
+// });
+    
 const input = document.getElementById('input');
 const form = document.getElementById('form');
 const message = document.getElementById('message');
@@ -20,8 +40,10 @@ form.addEventListener('submit', (e) => {
     //         alert("Veuillez saisir au moins 5 caractères pour votre message");
     //     }
     else if(input.value !== '' && message.value !== null ){
-            alert("Nous avons bien reçu votre message. Merci de nous avoir contacté !");
+            alert (" Merci " +input.value + " pour votre message. Nos équipes reviendront très vite vers vous !😂");
+            input, button, form.reset();
         }
-// else alert("votre message n'est pas complet");
+// else alert("votre message n'est pas complet") ("Votre message a été envoyé avec succés. Merci de nous avoir contacté !");
 });
     
+
