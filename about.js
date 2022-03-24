0793fc1120feaf79382993746ee8bc75be1dbb7b
JVRasa <j.rasamoelina@gmail.com>
2022-03-24 12:04:39 +0100

    night mode about

--- about.js
+++ about.js
@@ -1,6 +1,11 @@
+const iconAbout = document.getElementById('iconAbout');
+
 function theme() {
   if (localStorage.getItem('darkmode') === 'activated') {
     document.body.classList.add('dark');
+    iconAbout.setAttribute('src', './assets/person-night.png');
+  } else {
+    iconAbout.setAttribute('src', './assets/person-orange.png');
   }
 }
 theme();
